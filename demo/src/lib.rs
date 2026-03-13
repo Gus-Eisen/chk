957036cc5c2629d06cc8019280d6889d33c37905
Gus-Eisen <guseisenberch@proton.me>
2026-03-13 13:55:20 -0500

    Deleted commented out code.

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -7,26 +7,30 @@ mod state;
 pub use state::*;
 
 use chk::{
-    RootInfo, FormItem, NumberVariant, Flow, Bumper, ChkTheme, AvatarIconStyle,
-    Display, Offset, Context, Screen, PageType, PageBuilder, Icons, AvatarContent,
-    Color, Theme, Form, Root, State, Review, Success, Message, Profile, FormSubmit,
+    Bumper, ChkTheme, Color, Context, Display, Flow, Form, FormItem, FormSubmit, Icons, Message,
+    NumberVariant, Offset, PageBuilder, PageType, Profile, Review, Root, RootInfo, Screen, State,
+    Success, Theme,
 };
 
-use chk::items::{ListItem, Action, TableItem};
+use chk::items::{Action, ListItem, TableItem};
 
 chk::run! { |_ctx: &mut Context| Orange }
 
 pub struct Orange;
 impl chk::App for Orange {
     fn roots(&self, ctx: &mut Context, theme: &Theme) -> Vec<RootInfo> {
-        vec![
-            RootInfo::icon(ctx, theme, Icons::Wallet, "Wallet", BitcoinHome::new(theme)),
-            // RootInfo::icon(ctx, theme, Icons::Messages, "Messages", MessagesHome::new(theme)),
-            // RootInfo::avatar(ctx, theme, AvatarContent::icon(Icons::Profile, AvatarIconStyle::Secondary), "Profile", MessagesHome::new(theme))
-        ]
+        vec![RootInfo::icon(
+            ctx,
+            theme,
+            Icons::Wallet,
+            "Wallet",
+            BitcoinHome::new(theme),
+        )]
     }
 
-    fn theme(&self) -> ChkTheme {ChkTheme::Dark(Color::from_hex("#eb343a", 255))}
+    fn theme(&self) -> ChkTheme {
+        ChkTheme::Dark(Color::from_hex("#eb343a", 255))
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -34,22 +38,32 @@ pub struct BitcoinHome;
 
 impl BitcoinHome {
     fn new(theme: &Theme) -> PageType {
-        let items = Transaction::test().iter().map(|t| {
-            let _task = t.clone();
-            let title = if t.is_received {"Received bitcoin"} else {"Sent bitcoin"};
-            let view = vec![Screen::new_builder(theme, ViewTransaction::new(t.clone()))];
-            ListItem::plain(title, &t.date, Some(&t.amount.usd), Some(Flow::new(view)))
-        }).collect();
+        let items = Transaction::test()
+            .iter()
+            .map(|t| {
+                let _task = t.clone();
+                let title = if t.is_received {
+                    "Received bitcoin"
+                } else {
+                    "Sent bitcoin"
+                };
+                let view = vec![Screen::new_builder(theme, ViewTransaction::new(t.clone()))];
+                ListItem::plain(title, &t.date, Some(&t.amount.usd), Some(Flow::new(view)))
+            })
+            .collect();
 
         let send = SendFlow::new(theme);
         let receive = vec![Screen::new_builder(theme, Receive::new())];
 
-        Root::new("Wallet",
+        Root::new(
+            "Wallet",
             vec![
                 Display::currency(12.50, "0.00001234 BTC"),
                 Display::list(None, items, None),
-            ], //Some(Flow::new(vec![Screen::new_builder(builder, TaskDetails)])))],
-            None, ("Receive".into(), Flow::new(receive)), Some(("Send".into(), Flow::from_form(send))),
+            ],
+            None,
+            ("Receive".into(), Flow::new(receive)),
+            Some(("Send".into(), Flow::from_form(send))),
         )
     }
 }
@@ -60,7 +74,10 @@ impl Receive {
         Box::new(|_: &Theme| {
             PageType::display(
                 "Receive bitcoin",
-                vec![Display::qr_code(&Address::generate(), "Scan to receive bitcoin.")],
+                vec![Display::qr_code(
+                    &Address::generate(),
+                    "Scan to receive bitcoin.",
+                )],
                 None,
                 Bumper::custom("Copy Address", Action::share(&Address::generate())),
                 Offset::Center,
@@ -73,7 +90,11 @@ pub struct ViewTransaction;
 impl ViewTransaction {
     pub fn new(transaction: Transaction) -> Box<dyn PageBuilder> {
         Box::new(move |_: &Theme| {
-            let direction = if transaction.is_received {"Received"} else {"Sent"};
+            let direction = if transaction.is_received {
+                "Received"
+            } else {
+                "Sent"
+            };
             PageType::display(
                 &format!("{direction} bitcoin"),
                 vec![Display::currency(12.50, &transaction.amount.btc)],
@@ -88,44 +109,79 @@ impl ViewTransaction {
 pub struct SendFlow;
 impl SendFlow {
     pub fn new(theme: &Theme) -> Form {
-        let closure = Box::new(move |_ctx: &mut Context, objects: &Vec<State>| {println!("Transaction {:?}", objects)}) as Box<dyn FormSubmit>;
+        let closure = Box::new(move |_ctx: &mut Context, objects: &Vec<State>| {
+            println!("Transaction {:?}", objects)
+        }) as Box<dyn FormSubmit>;
 
         let review = |objects: &Vec<State>| {
-            let State::Text(address) = objects[0].clone() else { todo!() };
+            let State::Text(address) = objects[0].clone() else {
+                todo!()
+            };
             let btc = "0.00001234";
-            let State::Number(usd) = &objects[1] else { todo!() };
-            let State::Enumerator(priority) = &objects[2] else { todo!() };
+            let State::Number(usd) = &objects[1] else {
+                todo!()
+            };
+            let State::Enumerator(priority) = &objects[2] else {
+                todo!()
+            };
             let fee = "$0.38";
             let total = "$12.30";
 
             vec![
-                Display::review("Confirm address", &address, "Bitcoin sent to the wrong address can never be recovered."),
-                Display::table("Confirm amount", vec![
-                    TableItem::new("Amount Sent (BTC)", btc),
-                    TableItem::new("Amount Sent", usd),
-                    TableItem::new("Transaction Speed", match priority { // probably should use an enum here
-                        0 => "Standard (~2 hours)",
-                        _ => "Priority (~30 minutes)"
-                    }),
-                    TableItem::new("Transaction Fee", fee),
-                    TableItem::new("Transaction Total", total),
-                ])
+                Display::review(
+                    "Confirm address",
+                    &address,
+                    "Bitcoin sent to the wrong address can never be recovered.",
+                ),
+                Display::table(
+                    "Confirm amount",
+                    vec![
+                        TableItem::new("Amount Sent (BTC)", btc),
+                        TableItem::new("Amount Sent", usd),
+                        TableItem::new(
+                            "Transaction Speed",
+                            match priority {
+                                // probably should use an enum here
+                                0 => "Standard (~2 hours)",
+                                _ => "Priority (~30 minutes)",
+                            },
+                        ),
+                        TableItem::new("Transaction Fee", fee),
+                        TableItem::new("Transaction Total", total),
+                    ],
+                ),
             ]
         };
 
         let success = |objects: Vec<State>| {
-            let amount = if let State::Number(x) = &objects[1] {x} else {"$0.00"};
+            let amount = if let State::Number(x) = &objects[1] {
+                x
+            } else {
+                "$0.00"
+            };
             (Icons::Bitcoin, format!("You sent {}", amount))
         };
-        
-        Form::new(theme, vec![
-            FormItem::text("Bitcoin address"), //, |a: String| Address::is_valid_address(a)),
-            FormItem::number("Bitcoin amount", NumberVariant::Currency), //, |v: f32| Wallet::contains_amount(v)), // change to NumberVariant::Currency
-            FormItem::enumerator("Transaction speed", vec![
-                ("Standard", "Arrives in ~2 hours\n$0.18 bitcoin network fee"),
-                ("Priority", "Arrives in ~30 minutes\n$0.32 bitcoin network fee"),
-            ]),
-        ], Some(Review::new("Confirm send", review)), Some(Success::new("Bitcoin sent", success)), closure)
+
+        Form::new(
+            theme,
+            vec![
+                FormItem::text("Bitcoin address"),
+                FormItem::number("Bitcoin amount", NumberVariant::Currency),
+                FormItem::enumerator(
+                    "Transaction speed",
+                    vec![
+                        ("Standard", "Arrives in ~2 hours\n$0.18 bitcoin network fee"),
+                        (
+                            "Priority",
+                            "Arrives in ~30 minutes\n$0.32 bitcoin network fee",
+                        ),
+                    ],
+                ),
+            ],
+            Some(Review::new("Confirm send", review)),
+            Some(Success::new("Bitcoin sent", success)),
+            closure,
+        )
     }
 }
 
@@ -136,16 +192,24 @@ impl MessagesHome {
         let messages = Message::tests();
         let message = messages[0].clone();
         let chat = vec![Screen::new_builder(theme, Chat::new(messages))];
-        let items = vec![ListItem::avatar(message.author.avatar(), &message.author.name, &message.message, None, Some(Flow::new(chat)))];
+        let items = vec![ListItem::avatar(
+            message.author.avatar(),
+            &message.author.name,
+            &message.message,
+            None,
+            Some(Flow::new(chat)),
+        )];
 
         let new_message = NewMessageFlow::new(theme);
-        Root::new("Messages",
+        Root::new(
+            "Messages",
             vec![Display::list(None, items, None)],
-            None, ("New Message".into(), Flow::from_form(new_message)), None,
+            None,
+            ("New Message".into(), Flow::from_form(new_message)),
+            None,
         )
     }
 }
-
 
 pub struct NewMessageFlow;
 impl NewMessageFlow {
@@ -154,18 +218,41 @@ impl NewMessageFlow {
             println!("New Message {:?}", objects);
         }) as Box<dyn FormSubmit>;
 
-        let items = Profile::more_tests().into_iter().map(|profile| ListItem::avatar(profile.avatar(), &profile.name, "did address here", None, None)).collect::<Vec<_>>();
-        Form::new(theme, vec![FormItem::search("Select recipient", items)], None, None, closure)
+        let items = Profile::more_tests()
+            .into_iter()
+            .map(|profile| {
+                ListItem::avatar(
+                    profile.avatar(),
+                    &profile.name,
+                    "did address here",
+                    None,
+                    None,
+                )
+            })
+            .collect::<Vec<_>>();
+        Form::new(
+            theme,
+            vec![FormItem::search("Select recipient", items)],
+            None,
+            None,
+            closure,
+        )
     }
 }
-
 
 pub struct Chat;
 impl Chat {
     pub fn new(messages: Vec<Message>) -> Box<dyn PageBuilder> {
         Box::new(move |_builder: &Theme| {
-            let profiles = messages.clone().into_iter().map(|m| m.author).collect::<HashSet<_>>().into_iter().collect::<Vec<_>>();
+            let profiles = messages
+                .clone()
+                .into_iter()
+                .map(|m| m.author)
+                .collect::<HashSet<_>>()
+                .into_iter()
+                .collect::<Vec<_>>();
             PageType::messaging(messages.clone(), profiles)
         })
     }
 }
+
